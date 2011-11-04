a117a4c44a8ceec6cef0928026d7d663f69dfeb3
Marian Pollzien <map@wafriv.de>
2011-11-04 17:01:57 +0100

    * fixed a double $ causing an error

--- dw/index.php
+++ dw/index.php
@@ -63,7 +63,7 @@ elseif ($con)
 	$sql = 'SELECT login_closed FROM dw_game';
 	$login_closed = lib_util_mysqlQuery($sql);
 
-	if ($user->getUID() and (($login_closed == 1 and $user->getGameRank() < 1) xor ($login_closed == 2 and $$user->getGameRank() != 2) xor !$checkid))
+	if ($user->getUID() and (($login_closed == 1 and $user->getGameRank() < 1) xor ($login_closed == 2 and $user->getGameRank() != 2) xor !$checkid))
 	{
 		setcookie('lid', $_COOKIE['lid'], time()-100, '', '.dynasty-wars.de');
 		setcookie('city', $city, time()-100, '', '.dynasty-wars.de');
