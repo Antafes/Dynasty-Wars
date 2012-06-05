fb3a9440db19bd774fbfb544006cebb9a86661e1
Marian Pollzien <map@wafriv.de>
2012-06-05 10:45:13 +0200

    + config.default.php, config.php: New system for the configuration files. Place new configuration values in config.default.php and your local configuration in config.php, but don't remove the require_once in the config.php.

---
+++ dw/lib/config.default.php
@@ -0,0 +1,15 @@
+<?php
+//access data for the database
+$server = 'localhost';
+$seruser = '';
+$serpw = '';
+$serdb = '';
+
+//enable/disable debug
+$debug = true;
+$firePHP_debug = false;
+$smarty_debug = false;
+
+//paths
+define(DIR_WS, 'http://localhost/dwars/dw');
+define(DIR_WS_INDEX, 'http://localhost/dwars/dw/index.php');
