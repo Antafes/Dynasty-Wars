9b79cdded2b18d245657fd61f3843220bcb86b36
friend8 <map@wafriv.de>
2013-05-15 22:35:26 +0200

    removed local dev sys config

--- dw/lib/config.php
+++ dw/lib/config.php
@@ -1,19 +1,2 @@
 <?php
 require_once(dirname(__FILE__).'/config.default.php');
-
-$GLOBALS['db'] = array(
-	'server' => 'localhost',
-	'user' => 'root',
-	'password' => 'd88e5c3',
-	'db' => 'dwars',
-	'charset' => 'utf8',
-);
-
-//enable/disable debug
-$debug = true;
-$firePHP_debug = false;
-$smarty_debug = false;
-
-//paths
-$GLOBALS['config']['dir_ws'] = 'http://localhost/dw_git_dev/dw';
-$GLOBALS['config']['dir_ws_index'] = 'http://localhost/dw_git_dev/dw/index.php';
